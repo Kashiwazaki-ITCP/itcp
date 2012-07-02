909ebdcfbc0ab4e242cc67a026d2aa0dc607739b
Kashiwazaki ITCP <itcp@kasix.co.jp>
2012-07-03 01:14:05 +0900

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Itcp::Application.config.secret_token = '1a60ad349d32254e5f7521007cab5206bab00c58c1c9c893ec9dc4dece258e0d280f4fc118c3244133bc3276a53556aa6667ff14a6a9e3200ca0a7a21eda86f4'
